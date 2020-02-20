18a367a82d2e1a593534393c9964e97559a4c1b0
Andrey Shilov <andrey.dry7.shilov@yandex.ru>
2020-02-20 15:55:03 +0300

    Add types and YouTubeService

---
+++ src/app/modules/search/services/youtube.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SearchListResponse } from '../types';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+  private endpoint = 'https://www.googleapis.com/youtube/v3/';
+
+  constructor(private readonly http: HttpClient) { }
+
+  searchVideos(limit: number): Observable<SearchListResponse> {
+    return this.http.get<SearchListResponse>(`${this.endpoint}search?key=${environment.youTube.apiKey}`
+      + `&order=date&part=snippet &type=video,id&maxResults=${limit}`
+    );
+  }
+}
---
+++ src/app/modules/search/types.ts
@@ -0,0 +1,41 @@
+interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
+
+interface Thumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface SearchListItem {
+  kind: string;
+  etag: string;
+  id: {
+    kind: string;
+    videoId: string;
+  };
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: Thumbnail;
+      medium: Thumbnail;
+      high: Thumbnail;
+    };
+    channelTitle: string;
+    liveBroadcastContent: string;
+  };
+}
+
+export interface SearchListResponse {
+  kind: string;
+  etag: string;
+  nextPageToken: string;
+  regionCode: string;
+  pageInfo: PageInfo;
+  items: SearchListItem[];
+}
