1b89e804e34d66487f916530c918ff33aebf1480
Andrey Shilov <andrey.dry7.shilov@yandex.ru>
2020-02-25 03:46:58 +0300

    Fix lint error

--- src/app/tests/mocks/spinner.ts
+++ src/app/tests/mocks/spinner.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mat-progress-spinner',
+  selector: 'mat-progress-spinner', // tslint:disable-line
   template: '[mat-progress-spinner]',
 })
 export class MatProgressSpinnerComponent {}
